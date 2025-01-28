40b710607cc83d770010fda0901a2f2c3d34e16b
dk-xy <92148260+dk-xy@users.noreply.github.com>
2025-01-28 23:08:20 +0100

    bossCompletion

--- src/views/Character/CharacterBosses.vue
+++ src/views/Character/CharacterBosses.vue
@@ -3,22 +3,46 @@
     <h2>Bosses</h2>
     <div v-for="(boss, index) in character.Bosses.BossList" :key="index">
       <h3>{{ boss.Name }}</h3>
-      <div v-for="(difficulty, diffIndex) in boss.Difficulty" :key="diffIndex">
+      <div v-for="(difficulty, diffIndex) in activeDifficulties(boss)" :key="diffIndex">
         <p>{{ difficulty.DifficultyName }}: {{ difficulty.DifficultyReset }}</p>
+        <p>Completion Status: <input type="checkbox" v-model="difficulty.CompletionStatus" @change="handleCompletionChange(difficulty)" /></p>
       </div>
-      <p>Completion Status: <input type="checkbox" v-model="boss.CompletionStatus" disabled /></p>
-      <p>Completion Date: {{ boss.CompletionDate }}</p>
     </div>
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { saveData } from '../../localStorageService'
+
 const props = defineProps({
   character: {
     type: Object,
     required: true
   }
 })
+
+function activeDifficulties(boss) {
+  return boss.Difficulty.filter(difficulty => difficulty.isActive)
+}
+
+function handleCompletionChange(difficulty) {
+  if (difficulty.CompletionStatus) {
+    difficulty.CompletionDate = new Date().toISOString().split('T')[0] // Set to current date in YYYY-MM-DD format
+  } else {
+    difficulty.CompletionDate = ''
+  }
+  saveBosses()
+}
+
+function saveBosses() {
+  const characters = JSON.parse(localStorage.getItem('mapleStoryData')).Legion.Characters
+  const characterIndex = characters.findIndex(c => c.Name === props.character.Name)
+  if (characterIndex !== -1) {
+    characters[characterIndex] = props.character
+    saveData({ Legion: { Characters: characters } })
+  }
+}
 </script>
 
 <style scoped>
