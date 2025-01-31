5693c602036ffb1bf587756f92a12331ebdfc14b
dk-xy <92148260+dk-xy@users.noreply.github.com>
2025-01-31 12:58:52 +0100

    complete all on bosses

--- src/views/Character/CharacterBosses.vue
+++ src/views/Character/CharacterBosses.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="character-bosses">
+  <div class="character-bosses-container">
     <h2>Bosses</h2>
-    <div v-for="(boss, index) in bossesWithActiveDifficulties" :key="index">
-      <BossBlock :boss="boss" :saveBosses="saveBosses" />
-    </div>
+
+<div class="complete-all-container">
+  <button @click="toggleDropdown">Complete All</button>
+  <div v-if="dropdownVisible" class="dropdown-menu">
+    <button @click="completeAll('Daily')">Daily</button>
+    <button @click="completeAll('Weekly')">Weekly</button>
   </div>
+</div>
+<div class="character-bosses">
+<div v-for="(boss, index) in bossesWithActiveDifficulties" :key="index">
+  <BossBlock :boss="boss" :saveBosses="saveBosses" />
+</div>
+</div>
+  </div>
+    
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { saveData } from '../../localStorageService'
 import BossBlock from '../../components/Character/Bosses/BossBlock.vue'
 
@@ -19,12 +30,35 @@ const props = defineProps({
   }
 })
 
+const dropdownVisible = ref(false)
+
+function toggleDropdown() {
+  dropdownVisible.value = !dropdownVisible.value
+}
+
 const bossesWithActiveDifficulties = computed(() => {
   return props.character.Bosses.BossList.filter(boss => 
     boss.Difficulty.some(difficulty => difficulty.isActive)
   )
 })
+function completeAll(type) {
+  const allCompleted = props.character.Bosses.BossList.every(boss => 
+    boss.Difficulty.every(difficulty => 
+      difficulty.DifficultyReset !== type || !difficulty.isActive || difficulty.CompletionStatus
+    )
+  )
 
+  props.character.Bosses.BossList.forEach(boss => {
+    boss.Difficulty.forEach(difficulty => {
+      if (difficulty.DifficultyReset === type && difficulty.isActive) {
+        difficulty.CompletionStatus = !allCompleted
+        difficulty.CompletionDate = !allCompleted ? new Date().toISOString().split('T')[0] : ''
+      }
+    })
+  })
+  saveBosses()
+  dropdownVisible.value = false
+}
 function saveBosses() {
   const characters = JSON.parse(localStorage.getItem('mapleStoryData')).Legion.Characters
   const characterIndex = characters.findIndex(c => c.Name === props.character.Name)
